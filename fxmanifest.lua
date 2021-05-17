b72ded8916b68fc44137fb96d15ff3987c2e93e6
visibait <33099726+visibait@users.noreply.github.com>
2021-05-17 11:01:15 +0200

    VB: Fixed some source errors

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'AlejandroMume'
+description 'Script for send to a webhook your current coords in a lot of formats'
+version 'Optimized by VisiBait'
+
+client_script 'client.lua'
+server_script 'server.lua'
